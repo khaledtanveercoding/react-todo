e3b40610d11fde7bb5fc99a3abbe76d7f8519035
Khaled Tanveer <khaledtanveer+gh@gmail.com>
2024-11-12 15:45:21 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {id: 1, title: "wake up one morning"},
+  {id: 2, title: "papa always told me"},
+  {id: 3, title: "I was the chosen one"},
+  {id: 4, title: "one in a million"}
+]
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <h1>Todo List here</h1>
+    <ul>
+      {todoList.map(List)}
+    </ul>
+    </div>
   )
 }
 
