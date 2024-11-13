b2b91a8b8dadd139dd423d7626ce24b8a6e4523b
Khaled Tanveer <khaledtanveer+gh@gmail.com>
2024-11-12 22:12:52 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 
 const todoList = [
-  {id: 1, title: "wake up one morning"},
+  {id: 1, title: "woke up one morning"},
   {id: 2, title: "papa always told me"},
   {id: 3, title: "I was the chosen one"},
   {id: 4, title: "one in a million"}
@@ -12,14 +11,17 @@ const todoList = [
 
 function App() {
 
+
   return (
     <div>
-    <h1>Todo List here</h1>
-    <ul>
-      {todoList.map(List)}
-    </ul>
+      <h1>Todo List here</h1>
+     <ul>
+       {todoList.map(function(item) {
+        return (<li key={item.id}>{item.title}</li>)
+       })}
+     </ul>
     </div>
   )
 }
 
-export default App
+export default App;
