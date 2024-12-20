6cddedf4a339424569d45d96f5546116f070414a
Khaled Tanveer <khaledtanveer+gh@gmail.com>
2024-12-19 19:48:20 -0500

    Update with lesson 3

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 
+const AddTodoForm = (props) => {
+    const handleAddTodo = (event) => {
+        // Prevent the default form submission behavior
+        event.preventDefault();
 
-const AddTodoForm = ()=> {
+        // Retrieve the title value from the input
+        const todoTitle = event.target.title.value;
+        props.onAddTodo(todoTitle)
+
+        // Log the todo title to the console
+        console.log(todoTitle);
+
+        // Reset the form
+        event.target.reset();
+    };
+
     return (
         <div>
-            <form>
-            <label htmlFor="Title"> Title: </label>
-            <input id="todoTitle" type="text" />
-            <button name="name" value="value" type="submit">Add</button>
+            <form onSubmit={handleAddTodo}>
+                <label htmlFor="todoTitle">Title: </label>
+                <input 
+                    id="todoTitle" 
+                    name="title" 
+                    type="text" 
+                />
+                <button 
+                    name="name" 
+                    value="value" 
+                    type="submit"
+                >
+                    Add
+                </button>
             </form>
         </div>
-
     );
-
-
 }
 
 export default AddTodoForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList"
-
+import { useState } from "react";
 
 function App() {
 
+ const [newTodo, setnewTodo] = useState();
 
   return (
     <div>
     <div>
       <h1>Todo List here</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo = {setnewTodo}/> 
+      <p>{newTodo}</p>
+
     </div>
     <div>
-      <TodoList />
+      <TodoList />  
     </div>
     </div>
   )
